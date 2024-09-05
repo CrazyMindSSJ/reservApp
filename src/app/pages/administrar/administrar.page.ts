16ba43fa70537676901f8218c51287c54544f7af
crazymindssj <seba.urrutia@duocuc.cl>
2024-09-05 12:47:24 -0400

    crud con agregar

--- src/app/pages/administrar/administrar.page.ts
+++ src/app/pages/administrar/administrar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-administrar',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministrarPage implements OnInit {
 
-  constructor() { }
+  persona = new FormGroup({
+    rut: new FormControl('',[Validators.required,Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
+    nombre: new FormControl('',[Validators.required,Validators.pattern("[a-z]{3,5}")]),
+    fecha_nacimiento: new FormControl('',[Validators.required]),
+    genero: new FormControl('',[Validators.required]),
+    tiene_equipo : new FormControl('No',[Validators.required ]),
+    nombre_equipo : new FormControl('',[])
+  });
+
+  usuarios:any[] = [];
+
+
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    this.usuarios = this.usuarioService.getUsuarios();
   }
 
+  registrar(){
+    //asi llamamos a una variable Jason
+    if(this.usuarioService.createUsuarios(this.persona.value)){
+      alert("USUARIO CREADO CON ÉXITO!");
+      this.persona.reset();
+    }else{
+      alert("ERROR! No se pudo crear el usuario!")
+    }
+  }
 }
